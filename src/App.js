d2c784b22bea36bb5d767936b5972b0e00de72c4
joaqoo555 <joaquincarrera2018@gmail.com>
2022-11-15 18:49:46 -0300

    search cards, clear cards

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header.jsx";
 function App() {
   const [characters, setCharacters] = useState([]);
   function onSearch(character) {
+    
     fetch(`https://rickandmortyapi.com/api/character/${character}`)
       .then((response) => response.json())
       .then((data) => {
@@ -14,13 +15,25 @@ function App() {
           window.alert("No hay personajes con ese ID");
         }
       });
+
   }
+  console.log(characters)
+    const onClose = (id) => {
+        const others = characters.filter((element)=>{
+          if(element.id !== id)
+          return element
+        })
+        setCharacters(
+          others
+        )
+    }
+
 
   return (
     <div className="App">
       <Header onSearch={onSearch} />
       <div>
-        <Cards characters={characters} />
+        <Cards characters={characters} onClose={onClose} />
       </div>
     </div>
   );
